407dbce6a85ed33304e731bc0a512d8b6acef140
guilherme <guilherme09112001@gmail.com>
2025-10-28 00:08:43 +0100

    changes

--- src/modules/Attorneys/attorneys.route.ts
+++ src/modules/Attorneys/attorneys.route.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from '../../middleware/authMiddleware';
 const attorneyController = new AttorneyController();
 
 export const attorneysRoutes = async (fastify: FastifyInstance) => {
-    fastify.get('/attorney', { preHandler: authMiddleware }, attorneyController.findAll);
+    fastify.get('/attorney', { preHandler: authMiddleware }, attorneyController.findAll as any);
     fastify.post('/attorney', { preHandler: authMiddleware }, attorneyController.register);
     fastify.put('/attorney', { preHandler: authMiddleware }, attorneyController.update);
     fastify.delete('/attorney', { preHandler: authMiddleware }, attorneyController.remove as any);
