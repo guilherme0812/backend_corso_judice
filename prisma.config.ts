1fd7764d64da868c3bd137a184db5e5fb4b24f49
guilherme <guilherme09112001@gmail.com>
2025-11-25 13:08:13 +0100

    changes

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,11 +3,11 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  engine: "classic",
-  datasource: {
-    url: env("DATABASE_URL"),
-  },
+  // migrations: {
+  //   path: "prisma/migrations",
+  // },
+  // engine: "classic",
+  // datasource: {
+  //   url: env("DATABASE_URL"),
+  // },
 });
--- src/prisma/prismaClient.ts
+++ src/prisma/prismaClient.ts
@@ -1,10 +1,9 @@
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-const adapter = new PrismaPg({ connectionString })
-const prismaClient = new PrismaClient({ adapter })
-
-export { prismaClient }
+export const prismaClient = new PrismaClient({ adapter });
